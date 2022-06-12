811537d8196710982903b608350c9eef04602e5c
Yasser Abady <yasser.abady@ryerson.ca>
2022-06-11 23:02:10 -0400

    buy and sell stock once

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,8 +4,20 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    # [ 50, 40, 30, 50, 20, 100, 10, 50] --> 80
+    # O(n) constant amount of work in array
+    # O(1) space since we are tracking the iterator, profit, and lowest vars
+
+    profit = 0
+    lowest = float('inf')
+
+    for price in prices:
+        if price < lowest:
+            lowest = price
+        
+        profit = max(profit, price-lowest)
+
+    return profit
 
 
 if __name__ == '__main__':
