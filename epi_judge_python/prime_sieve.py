996f71fb00e9c766ae1ecd359b06a44462286d47
Yasser Abady <yassera@Yassers-MacBook-Pro.local>
2022-06-20 01:56:42 -0400

    prime coiunt

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -5,8 +5,18 @@ from test_framework import generic_test
 
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    # We are going to use the canidete approuch where we will store already itrated primes and update later on tog et faster solution
+    primes = [False, False] + [True] * (n-1)
+
+    for i in range(n+1):
+        if primes[i]:
+            result.append(i)
+            for j in range(i, n+1, i):
+
+                primes[j] = False
+
+    return result
 
 
 if __name__ == '__main__':
