12cd91bc07cae3fd0d89149e968a1f27a2a34841
Yasser Abady <yasser.abady@ryerson.ca>
2022-07-12 23:03:05 -0400

    replace_and_remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -7,22 +7,34 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size: int, s: List[str]) -> int:
-    real_index, a_count = 0,0
+    write_index, a_count = 0, 0 
+
     for i in range(size):
-        if s[i] != "b":
-            s[real_index] = s[i]
-            real_index += 1
+        if s[i] != 'b':
+            s[write_index] = s[i]
+            write_index += 1
+
         if s[i] == 'a':
             a_count += 1
 
-    print(size)
-    print(real_index)
-    print(s)
-    print(a_count)
-    print("testrtt")
+    curr_index = write_index - 1
 
+    write_index += a_count - 1
 
-    return 0
+    final_size = write_index + 1
+
+    while curr_index >= 0:
+        if s[curr_index] == 'a':
+            s[write_index] = 'd'
+            s[write_index - 1] = 'd'
+            write_index -= 2
+        else:
+            s[write_index] = s[curr_index]
+            write_index -= 1
+
+        curr_index -= 1
+
+    return final_size
 
 
 @enable_executor_hook
